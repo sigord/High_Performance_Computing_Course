ce598a80d64ffd632da867829de9674ba3deb562
Igor Sudin <sudin.work@gmail.com>
2022-10-12 02:16:51 +0300

    Matmul done

--- HWs/HW1/matmul.c
+++ HWs/HW1/matmul.c
@@ -126,6 +126,70 @@ double CalcMatMulTime_kij_opt(double * A, double * B, double * C, size_t N)
     return r_time;
 }
 
+
+double CalcMatMulTime_My_first(double * A, double * B, double * C, size_t N)
+{
+    struct timeval start, end;
+    double r_time = 0.0;
+    size_t i, j, k;
+
+    size_t dummy = 0;
+
+    ZeroMatrix(&C[0], N);
+
+    gettimeofday(&start, NULL);
+    
+    for (k = 0; k < N; k++)
+        for(i = 0; i < N; i++)
+        {
+            dummy = i * N;
+            for(j = 0; j < N; j++)
+                C[dummy + j] += A[dummy + k] * B[k * N + j];
+        }
+    gettimeofday(&end, NULL);
+    
+    r_time = end.tv_sec - start.tv_sec + ((double) (end.tv_usec - start.tv_usec)) / 1000000;
+    
+    return r_time;
+}
+
+
+double CalcMatMulTime_My_second(double * A, double * B, double * C, size_t N)
+{
+    struct timeval start, end;
+    double r_time = 0.0;
+    size_t i, j, k, i2, j2, k2;
+    double * leftmul, * rightmul, * finalres;
+    int len = (64/sizeof(double));
+
+    size_t dummy = 0;
+
+    ZeroMatrix(&C[0], N);
+
+    gettimeofday(&start, NULL);
+    
+    for (i = 0; i < N; i += len)
+    {
+        dummy = i * N;
+        for (j = 0; j < N; j += len)
+            for (k = 0; k < N; k += len)
+                for (i2 = 0, finalres = &C[dummy + j],
+                     leftmul = &A[dummy + k]; i2 < len;
+                     ++i2, finalres += N, leftmul += N)
+                    for (k2 = 0, rightmul = &B[k * N + j];
+                         k2 < len; ++k2, rightmul += N)
+                        for (j2 = 0; j2 < len; ++j2)
+                            finalres[j2] += leftmul[k2] * rightmul[j2];
+    }
+
+    gettimeofday(&end, NULL);
+    
+    r_time = end.tv_sec - start.tv_sec + ((double) (end.tv_usec - start.tv_usec)) / 1000000;
+    
+    return r_time;
+}
+
+
 int main()
 {
     
@@ -189,6 +253,30 @@ int main()
         average_runtime += runtimes[n]/NRuns;
     }
     printf("average runtime kij opt %lf seconds\n", average_runtime);
+    printf("---------------------------------\n");
+
+
+    // My realization first
+    average_runtime = 0.0;
+    for(int n=0; n<NRuns; n++)
+    {
+        runtimes[n]=CalcMatMulTime_My_first(&A[0], &B[0], &C[0], N);
+        printf("runtime %lf seconds\n", runtimes[n]);
+        average_runtime += runtimes[n]/NRuns;
+    }
+    printf("average runtime my first realization %lf seconds\n", average_runtime);
+    printf("---------------------------------\n");
+
+
+    // My realization second
+    average_runtime = 0.0;
+    for(int n=0; n<NRuns; n++)
+    {
+        runtimes[n]=CalcMatMulTime_My_second(&A[0], &B[0], &C[0], N);
+        printf("runtime %lf seconds\n", runtimes[n]);
+        average_runtime += runtimes[n]/NRuns;
+    }
+    printf("average runtime my second realization %lf seconds\n", average_runtime);
     printf("---------------------------------\n");
 
     free(A); 
